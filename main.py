fe12b70ed6ae3886018a8cc4e21068c0ddedad7b
위정우 <80455444+yellow0104@users.noreply.github.com>
2021-10-02 13:11:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -70,11 +70,11 @@ async def 유저정보(ctx, user: discord.Member = None):
 @bot.command()
 async def 도움말(ctx):
     embed = discord.Embed(title="도움말", color=0x00ffff)
-    embed.add_field(name="접두사", value="?")
-    embed.add_field(name="공지", value="공지를 띄어줍니다.")
-    embed.add_field(name="청소", value="청소 (수) 채팅청소를 해줍니다")
-    embed.add_field(name="유저정보", value="유저정보를 띄어줍니다.")
-    embed.set_footer(text="이 봇은 discord 모듈 공부용입니다.")
+    embed.add_field(name="접두사", value="?", inline=False)
+    embed.add_field(name="공지", value="공지를 띄어줍니다.", inline=False)
+    embed.add_field(name="청소", value="청소 (수) 채팅청소를 해줍니다", inline=False)
+    embed.add_field(name="유저정보", value="유저정보를 띄어줍니다.", inline=False)
+    embed.set_footer(text="이 봇은 discord 모듈 공부용입니다")
     await ctx.send(embed=embed)
     
 bot.run(token)
