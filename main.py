287718a7bef1ab21172625a1e5782f31fb5590d3
위정우 <80455444+yellow0104@users.noreply.github.com>
2021-10-04 22:26:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #beta
 from discord.ext import commands
 import discord
+from datetime import datetime
+
+from discord.utils import get
 
 
+now = datetime.now()
+now_day = f"{now.year}/{now.month}/{now.day}"
 
 token = ""
 intents = discord.Intents.all() 
@@ -15,21 +20,46 @@ async def on_ready():
     print(bot.user)
     game = discord.Game("?도움말")
     await bot.change_presence(status=discord.Status.online, activity=game)
+def find_first_channel(channels):
+    position_array = [i.position for i in channels]
 
+    for i in channels:
+        if i.position == min(position_array):
+            return i
+
+@bot.event
+
+async def on_member_join(member):
+    msg = "<@{}>님이 서버에 들어오셨어요. 환영합니다.".format(str(member.id))
+    await find_first_channel(member.guild.text_channels).send(msg)
+    await member.add_roles(get(member.guild.roles, name="members"))
+    return None
+@bot.event
+async def on_member_remove(member):
+    msg = f"{member}님께서 나가셨어요 **Good bye**"
+    await find_first_channel(member.guild.text_channels).send(msg)
+
+
+@bot.command()
+async def 준성(ctx):
+    await ctx.send("**멍청이**")
 
 
 #공지를 보내는 명령어
 @bot.command()
-async def 공지(ctx):
+async def 공지(ctx, contents: str = None, contents2: str = None):
     if ctx.author.guild_permissions.manage_messages:
-
-        content = ctx.message.content[3:]
-        await ctx.channel.purge(limit=1)
-        embed = discord.Embed(title="공지📢", description=content, color=0x00ff00)
-        embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
-        msg = await ctx.send(embed=embed)
-        await msg.add_reaction("👍")
-        await msg.add_reaction("👎")
+        try:
+            await ctx.channel.purge(limit=1)
+            embed = discord.Embed(title="공지📢", color=0x00ff00)
+            embed.add_field(name=contents, value=contents2)
+            embed.set_footer(text=now_day, icon_url=ctx.author.avatar_url)
+            msg = await ctx.send(embed=embed)
+            await msg.add_reaction("👍")
+            await msg.add_reaction("👎")
+        except:
+            embed = discord.Embed(title="erro!", description="명령어 사용 예시:\n?공지 (제목) (내용)", color=0xff0000)
+            await ctx.send(embed=embed)
     else:
         await ctx.send("권한이 없습니다.")
 #채팅 청소 명령어
@@ -38,7 +68,10 @@ async def 청소(ctx):
     if ctx.author.guild_permissions.manage_messages: #관리자 권한이 있는가?
         try:
             number = int(ctx.message.content[3:])
-            await ctx.channel.purge(limit=number)
+            if number >= 100:
+                await ctx.send("99까지만 청소가능해요")
+            else:
+                await ctx.channel.purge(limit=number)
         except:
             embed = discord.Embed(title="erro!", description="명령어 사용 예시:\n?청소 10", color=0xff0000)
             await ctx.send(embed=embed)
@@ -67,6 +100,7 @@ async def 유저정보(ctx, user: discord.Member = None):
     except:
             embed = discord.Embed(title="erro!", description="명령어 사용 예시:\n?유저정보 @정우", color=0xff0000)
             await ctx.send(embed=embed)
+
 @bot.command()
 async def 도움말(ctx):
     embed = discord.Embed(title="도움말", color=0x00ffff)
@@ -74,7 +108,13 @@ async def 도움말(ctx):
     embed.add_field(name="공지", value="공지를 띄어줍니다.", inline=False)
     embed.add_field(name="청소", value="청소 (수) 채팅청소를 해줍니다", inline=False)
     embed.add_field(name="유저정보", value="유저정보를 띄어줍니다.", inline=False)
+    embed.add_field(name="ping", value="pong!", inline=False)
     embed.set_footer(text="이 봇은 discord 모듈 공부용입니다")
     await ctx.send(embed=embed)
-    
+@bot.command()
+async def ping(ctx):
+    embed = discord.Embed(title="pong!🏓", color=0x00ff00)
+    embed.set_thumbnail(url="http://gifimgs.com/animations/sports/tennis-and-ping-pong/Ping-Pong.gif")
+    embed.add_field(name="현재핑 : ", value=f"{bot.latency * 1000}ms")
+    await ctx.send(embed=embed)
 bot.run(token)
